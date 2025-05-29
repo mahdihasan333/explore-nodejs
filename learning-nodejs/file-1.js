cb27ab92e012431c542fb8c254a207341c573cc5
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-30 00:48:07 +0600

    Explore Commonjs Module

---
+++ learning-nodejs/file-1.js
@@ -0,0 +1,11 @@
+// const var1 =  require("./file-2.js")
+const {a, add, b} =  require("./file-2.js")
+
+
+// console.log(var1)
+// console.log(var1.a)
+// console.log(var1.add(2, 3))
+
+console.log(a)
+console.log(add(5, 10))
+console.log(b)
---
+++ learning-nodejs/file-2.js
@@ -0,0 +1,15 @@
+const a = 10;
+
+const add = (param1, param2) => param1 + param2
+const b = 20;
+
+// module.exports = a
+// module.exports = add
+
+module.exports = {
+    a,
+    add,
+    b
+}
+
+// console.log(module)
