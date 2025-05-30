343a08376b5d89d7c8741cf13201860a9d3d9179
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-30 23:07:16 +0600

    Name Exports, Name Aliasing & Index Export

--- learning-nodejs/file-1.js
+++ learning-nodejs/file-1.js
@@ -1,5 +1,6 @@
 // const var1 =  require("./file-2.js")
 const {a, add, b} =  require("./file-2.js")
+const {a : a3, add : add3, b : b3} = require("./file-3.js")
 
 
 // console.log(var1)
@@ -9,3 +10,7 @@ const {a, add, b} =  require("./file-2.js")
 console.log(a)
 console.log(add(5, 10))
 console.log(b)
+
+console.log(a3)
+console.log(add3(5, 5, 5))
+console.log(b3)
---
+++ learning-nodejs/main.js
@@ -0,0 +1,8 @@
+// const {add} = require('./utils/add')
+// const {subtract} = require('./utils/subtract')
+
+// const {add, subtract} = require('./utils/index')
+const {add, subtract} = require('./utils')
+
+console.log(add(3, 2))
+console.log(subtract(3, 2))
