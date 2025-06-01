52d85829f4ef24990d4e035d7996fc7f99070263
Mahdi Hassan <mahdioffi333@gmail.com>
2025-06-01 16:55:06 +0600

    Routing In Node Js

--- module-2/ToDo-App/server.js
+++ module-2/ToDo-App/server.js
@@ -1,10 +1,27 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log({req, res});
-    res.end('Welcome to ToDo App Server')
+    // console.log({req, res});
+    // console.log(req.url, req.method)
+    // res.end('Welcome to ToDo App Server')
+    if(req.url === '/todos' && req.method === "GET"){
+        res.end("All Todos")
+    }
+    else if(req.url === '/todos/create-todo' && req.method === "POST"){
+        res.end('Todo Created')
+    }
+    else{
+        res.end("Route Not Found")
+    }
 })
 
 server.listen(5000, '127.0.0.1', () => {
     console.log('✅ Server listening to port 5000')
 })
+
+
+
+/**
+ * /todos - GET - ALL Todo
+ * /todos/create-todo POST Create Todo
+ */
