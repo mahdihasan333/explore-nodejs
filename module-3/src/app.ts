863e0c354a91fe49bef3b74859cc5cdc4d610646
Mahdi Hassan <mahdioffi333@gmail.com>
2025-06-03 15:23:14 +0600

    Explore What is Parsers, Request And Response Object

--- module-3/src/app.ts
+++ module-3/src/app.ts
@@ -1,12 +1,26 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Request, Response } from 'express';
+import fs from 'fs';
+import path from 'path';
 const app : Application = express()
+
+app.use(express.json())
+
+const filePath = path.join(__dirname, "../db/todo.json");
 
 
 app.get('/', (req : Request, res : Response) => {
-  res.send('Hello World!')
+  res.send('Welcome to Todos App!')
 })
 app.get('/todos', (req : Request, res : Response) => {
-  res.send('Hello World!')
+  const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+  console.log(data)
+  res.json(data)
+})
+
+app.post('/todos/create-todo', (req: Request, res: Response) => {
+  const {title, body} = req.body;
+  console.log(title, body)
+  res.send('Hello World')
 })
 
 
