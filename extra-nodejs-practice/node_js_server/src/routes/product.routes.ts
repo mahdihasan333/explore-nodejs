21a3373f8801ebd232934a51eb381de4b03e8b68
Mahdi Hassan <mahdioffi333@gmail.com>
2025-06-07 20:16:46 +0600

    practice

--- extra-nodejs-practice/node_js_server/src/routes/product.routes.ts
+++ extra-nodejs-practice/node_js_server/src/routes/product.routes.ts
@@ -4,8 +4,8 @@ export const routeHandler = async(req: IncomingMessage, res: ServerResponse) => 
     const url = req.url;
     if(url === '/' && req.method === 'GET'){
         res.writeHead(200, {'Content-type': 'text/plain'})
-        res.end('Welcome to Nodejs Server!!!')
+        res.end('Welcome to Node js Server!!!')
     }else{
-        
+        console.log('node')
     }
 }
