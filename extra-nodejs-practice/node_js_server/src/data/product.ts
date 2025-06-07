32697bac55f25c5d70b69e93054223cf0c94207a
Mahdi Hassan <mahdioffi333@gmail.com>
2025-06-07 20:18:51 +0600

    practice

--- extra-nodejs-practice/node_js_server/src/data/product.ts
+++
@@ -1,6 +0,0 @@
-import { IProduct } from "../types/product";
-
-export let products : IProduct[] = [
-    {id: 1, name: 'pen'},
-    {id: 2, name: 'Notebook'}
-]
--- extra-nodejs-practice/node_js_server/src/types/product.ts
+++
@@ -1,4 +0,0 @@
-export interface IProduct{
-    id: number;
-    name: string;
-}
