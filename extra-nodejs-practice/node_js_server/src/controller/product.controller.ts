c90e6302f2d10e2a0918761562d6535e1c50e9d2
Mahdi Hassan <mahdioffi333@gmail.com>
2025-06-08 16:23:52 +0600

    node js post request practice

--- extra-nodejs-practice/node_js_server/src/controller/product.controller.ts
+++ extra-nodejs-practice/node_js_server/src/controller/product.controller.ts
@@ -1,5 +1,7 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { products } from "../data/product";
+import { parseBody } from "../utils/parseBody";
+import { IProduct } from "../types/product";
 
 export const handleProducts = async(req : IncomingMessage, res : ServerResponse) => {
     const url = req.url
@@ -7,5 +9,17 @@ export const handleProducts = async(req : IncomingMessage, res : ServerResponse)
     if(url === '/products' && req.method === 'GET'){
         res.writeHead(200, {'content-type' : 'application/json'})
         res.end(JSON.stringify(products))
+    }
+    else if(url === '/products' && req.method === 'POST'){
+        try {
+            const body =  await parseBody(req)
+            const newProduct : IProduct = {id: 3, name: body.name}
+            products.push(newProduct)
+            res.writeHead(201, {'content-type' : 'application/json'})
+            res.end(JSON.stringify(products))
+        } catch (err) {
+            res.writeHead(400)
+            res.end('Error Occurred')
+        }
     }
 }
