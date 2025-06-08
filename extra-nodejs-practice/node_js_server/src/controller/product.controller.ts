eef01999ce5787ba25f6a47d9553489f54587446
Mahdi Hassan <mahdioffi333@gmail.com>
2025-06-08 21:46:02 +0600

    node js PUT request practice

--- extra-nodejs-practice/node_js_server/src/controller/product.controller.ts
+++ extra-nodejs-practice/node_js_server/src/controller/product.controller.ts
@@ -1,10 +1,12 @@
 import { IncomingMessage, ServerResponse } from "http";
-import { products } from "../data/product";
+import { products } from '../data/product';
 import { parseBody } from "../utils/parseBody";
 import { IProduct } from "../types/product";
 
 export const handleProducts = async(req : IncomingMessage, res : ServerResponse) => {
     const url = req.url
+    const idMatch = url?.match(/\/products\/(\d+)/);
+    const id = idMatch ? Number(idMatch[1]) : null;
 
     if(url === '/products' && req.method === 'GET'){
         res.writeHead(200, {'content-type' : 'application/json'})
@@ -22,4 +24,19 @@ export const handleProducts = async(req : IncomingMessage, res : ServerResponse)
             res.end('Error Occurred')
         }
     }
+    else if(idMatch && req.method === 'PUT'){
+        const body = await parseBody(req)
+        const index = products.findIndex(p => p.id === id)
+        if(index !== -1){
+            products[index].name = body.name;
+            res.writeHead(200, {'content-type' : 'application/json'})
+            res.end(JSON.stringify(products))
+        }
+        else{
+            res.writeHead(404)
+            res.end('Product Not Found')
+        }
+    }
+
+
 }
