0e4dfe9becefef2387abd1c367b9d63ba985f745
Mahdi Hassan <mahdioffi333@gmail.com>
2025-06-18 20:25:15 +0600

    More About Built-In Validation, Making Custom Validatins & Third Party Validator Package

--- advanced-note-app/src/app/models/user.model.ts
+++ advanced-note-app/src/app/models/user.model.ts
@@ -1,48 +1,59 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interface";
-
+import validator from 'validator'
 
 const userSchema = new Schema<IUser>({
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: [3, 'First Name must be 3 characters, got {VALUE}'],
-        maxlength: 10
+  firstName: {
+    type: String,
+    required: [true, "FirstName keno daw nai"],
+    trim: true,
+    minlength: [3, "First Name must be 3 characters, got {VALUE}"],
+    maxlength: 10,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: [3, "Last Name must be 3 characters, got {VALUE}"],
+    maxlength: 10,
+  },
+  age: {
+    type: Number,
+    required: true,
+    trim: true,
+    // min: 21,
+    min: [21, "Age Must be at least 18, got {VALUE}"],
+    max: 60,
+  },
+  email: {
+    type: String,
+    unique: [true, "Email common hoye geche!!"],
+    required: true,
+    lowercase: true,
+    trim: true,
+    // validate: {
+    //   validator: function (value) {
+    //     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+    //   },
+    //   message: function (props) {
+    //     return `Email ${props.value} is not valid email`;
+    //   },
+    // },
+    validate: [validator.isEmail, 'Invalid Email {VALUE}']
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    uppercase: true,
+    enum: {
+      values: ["USER", "ADMIN", "SUPERADMIN "],
+      message: "Role is not valid. got {VALUE} role",
     },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: [3, 'Last Name must be 3 characters, got {VALUE}'],
-        maxlength: 10
-        
-    },
-    age: {
-        type: Number,
-        required: true,
-        trim: true,
-        // min: 21,
-        min: [21, 'Age Must be at least 18, got {VALUE}'],
-        max: 60
-    },
-    email: {
-        type : String,
-        unique: true,
-        required: true,
-        lowercase: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        uppercase: true,
-        enum: ['USER', 'ADMIN', 'SUPERADMIN '],
-        default: 'USER'
-    }
-})
+    default: "USER",
+  },
+});
 
-export const User = model('User', userSchema)
+export const User = model("User", userSchema);
